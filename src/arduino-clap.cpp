ba9869dedd9147be6f25648683d5fef496ce6f3d
Harvey Bates <phenobottle@gmail.com>
2022-11-13 14:20:58 +1100

    initial commit

---
+++ src/arduino-clap.cpp
@@ -0,0 +1,157 @@
+#include "arduino-clap.h"
+
+static char cmd_buffer[ArduinoCLI::MAX_CMD_BUF_LEN];
+
+CL_Command::CL_Command(const char* _name, bool _takes_value,
+                       const char* _help_info, void (*_callback)()){
+
+    assert(strlen(_name) < MAX_ARG_LEN);
+    assert(strlen(_help_info) < MAX_HELP_LEN);
+
+    strncpy(name, _name, MAX_ARG_LEN);
+    takes_value = _takes_value;
+    strncpy(help, _help_info, MAX_HELP_LEN);
+
+    callback = _callback;
+}
+
+CL_Command::CL_Command(const char* _name, const char* _help_info,
+                       CL_Command* _nested_command){
+
+    assert(strlen(_name) < MAX_ARG_LEN);
+    assert(strlen(_help_info) < MAX_HELP_LEN);
+
+    strncpy(name, _name, MAX_ARG_LEN);
+    strncpy(help, _help_info, MAX_HELP_LEN);
+
+    has_child = true;
+
+    if(n_sub_commands < MAX_NEXTED_DEPTH){
+        sub_commands[n_sub_commands++] = _nested_command;
+    }
+}
+
+CL_Command::CL_Command(const char* _name, const char* _help_info){
+    assert(strlen(_name) < MAX_ARG_LEN);
+    assert(strlen(_help_info) < MAX_HELP_LEN);
+
+    strncpy(name, _name, MAX_ARG_LEN);
+    strncpy(help, _help_info, MAX_HELP_LEN);
+}
+
+void CL_Command::add_sub_command(CL_Command* _nested_command){
+    has_child = true;
+
+    if(n_sub_commands < MAX_NEXTED_DEPTH){
+        sub_commands[n_sub_commands++] = _nested_command;
+    }
+}
+
+void ArduinoCLI::add_command(CL_Command *command) {
+    if(n_commands < MAX_COMMANDS){
+        commands[n_commands++] = command;
+    }
+}
+
+CL_Command* ArduinoCLI::scan_commands(const char* input){
+
+    for(uint8_t i = 0; i < n_commands; i++){
+        if(strcmp(input, commands[i]->name) == 0){
+            if(!commands[i]->has_child){
+                commands[i]->callback();
+            }
+            return commands[i];
+        }
+    }
+
+    serial.printf("Unknown service request: %s\n", input);
+    return nullptr;
+}
+
+CL_Command* ArduinoCLI::scan_command(const char* input,
+                                     const CL_Command* parent_command){
+
+    if(strcmp(input, "help") == 0){
+        help(parent_command);
+        return nullptr;
+    }
+
+    for(uint8_t i = 0; i < parent_command->n_sub_commands; i++){
+        if(strcmp(input, parent_command->sub_commands[i]->name) == 0){
+            if(!parent_command->sub_commands[i]->has_child){
+                parent_command->sub_commands[i]->callback();
+            }
+            return parent_command->sub_commands[i];
+        }
+    }
+
+    serial.print("Unknown command: ");
+    serial.println(input);
+
+    return nullptr;
+}
+
+void ArduinoCLI::help(const CL_Command* command){
+    serial.println("USAGE:");
+    serial.print("\t");
+    serial.print(command->name);
+    serial.println(" [OPTIONS]");
+
+    if(command->n_sub_commands == 0){
+        serial.print("Help not available for command: ");
+        serial.println(command->name);
+        return;
+    }
+
+    serial.println("OPTIONS:");
+    for(uint8_t i = 0; i < command->n_sub_commands; i++){
+        serial.print("\t");
+        serial.print(command->sub_commands[i]->name);
+        serial.print("\t");
+        serial.println(command->sub_commands[i]->help);
+    }
+}
+
+bool ArduinoCLI::exit(const char* input){
+    if(strcmp(input, "exit") == 0){
+        serial.println("Exiting command line.");
+        return true;
+    }
+    return false;
+}
+
+void ArduinoCLI::enter(){
+    serial.print("$ ");
+    bool exit_cli = false;
+    while(!exit_cli) {
+        if(serial.available()){
+            serial.readBytesUntil('\n', cmd_buffer, sizeof(cmd_buffer));
+            serial.println(cmd_buffer);
+
+            // TODO Needs some assertion that the command is correct
+
+            const char* delim_cmd = strtok(cmd_buffer, " ");
+            CL_Command* parent_argument = nullptr;
+            while(delim_cmd != NULL){
+                exit_cli = exit(delim_cmd);
+                if(exit_cli){
+                    break;
+                }
+                if(parent_argument == nullptr){
+                    parent_argument = scan_commands(delim_cmd);
+                    if(parent_argument == nullptr){
+                        break;
+                    }
+                } else {
+                    parent_argument = scan_command(delim_cmd,
+                                                   parent_argument);
+                }
+                delim_cmd = strtok(NULL, " ");
+            }
+            serial.print("$ ");
+        }
+
+        memset(cmd_buffer, 0, sizeof(cmd_buffer));
+        delay(1);
+    }
+}
---
+++ src/arduino-clap.h
@@ -0,0 +1,67 @@
+#ifndef ARDUINO_CLAP_H
+#define ARDUINO_CLAP_H
+
+#include <Arduino.h>
+
+class CL_Command{
+    // Constants
+    static const uint8_t MAX_ARG_LEN        =       20;
+    static const uint8_t MAX_NEXTED_DEPTH   =       10;
+    static const uint8_t MAX_HELP_LEN       =       UINT8_MAX;
+
+public:
+    // Commands with function callback
+    CL_Command(const char* _name, bool _takes_value, const char* _help_info,
+               void (*_callback)());
+    // Commands with nested command
+    CL_Command(const char* _name, const char* _help_info,
+               CL_Command* _nested_command);
+    // Command with multiple nested command
+    CL_Command(const char* _name, const char* _help_info);
+
+    // Add a sub command to an existing command
+    void add_sub_command(CL_Command* _nested_command);
+
+    // Full name
+    char name[MAX_ARG_LEN]{}; // Full name
+    // Has a value passed to it
+    bool takes_value = false; // Has value
+    // Help information
+    char help[MAX_HELP_LEN]{}; // Help information
+    // Has a sub-command
+    bool has_child = false; // Has at least one sub-command
+    // Number of nested commands
+    uint8_t n_sub_commands = 0;
+    // List of nested commands
+    CL_Command* sub_commands[MAX_NEXTED_DEPTH]{};
+    // Command callback function
+    void (*callback)(){};
+};
+
+class ArduinoCLI {
+    // Constants
+    static const uint8_t MAX_NAME_LEN       =       20;
+    static const uint8_t MAX_COMMANDS       =       3;
+
+    uint8_t n_commands                      =       0;
+    CL_Command* commands[MAX_COMMANDS]{};
+
+    Stream& serial;
+
+public:
+    explicit ArduinoCLI(Stream& _serial) : serial(_serial) {}
+    ~ArduinoCLI() = default;
+
+    static const uint8_t MAX_CMD_BUF_LEN    =       UINT8_MAX;
+
+    void add_command(CL_Command* command);
+    CL_Command* scan_commands(const char* input);
+    CL_Command* scan_command(const char* input,
+                             const CL_Command* parent_command);
+    void help(const CL_Command* command);
+    bool exit(const char* input);
+    void enter();
+};
+
+
+#endif //ARDUINO_CLAP_H
